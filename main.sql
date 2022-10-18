91679a7edc592a1f95cd495467a4633b33580b95
KittipasM <111644551+KittipasM@users.noreply.github.com>
2022-10-18 21:46:38 +0700

    Correct comment sign

--- main.sql
+++ main.sql
@@ -1,11 +1,11 @@
-# Create an answer table to hold the results.
+-- Create an answer table to hold the results.
 CREATE TABLE answer (
   product_class_name TEXT,
   rank INTEGER,
   product_name TEXT,
   sales_value INTEGER
   )
-# Add the results from Class A to the answer table
+-- Add the results from Class A to the answer table
 INSERT INTO answer
 SELECT product_class_name, DENSE_RANK() OVER (ORDER BY (retail_price*quantity) DESC) rank, product_name, (retail_price*quantity) as sales_value
 FROM Product p
@@ -14,7 +14,7 @@ INNER JOIN SalesTransaction st ON p.product_id = st.product_id
 WHERE product_class_name = "Class A"
 ORDER BY product_class_name, (retail_price*quantity) DESC, quantity
 LIMIT 2
-# Add the results from Class B to the answer table
+-- Add the results from Class B to the answer table
 INSERT INTO answer
 SELECT product_class_name, DENSE_RANK() OVER (ORDER BY (retail_price*quantity) DESC) rank, product_name, (retail_price*quantity) as sales_value
 FROM Product p
@@ -23,7 +23,7 @@ INNER JOIN SalesTransaction st ON p.product_id = st.product_id
 WHERE product_class_name = "Class B"
 ORDER BY product_class_name, (retail_price*quantity) DESC, quantity
 LIMIT 2
-# Add the results from Class C to the answer table
+-- Add the results from Class C to the answer table
 INSERT INTO answer
 SELECT product_class_name, DENSE_RANK() OVER (ORDER BY (retail_price*quantity) DESC) rank, product_name, (retail_price*quantity) as sales_value
 FROM Product p
@@ -32,6 +32,6 @@ INNER JOIN SalesTransaction st ON p.product_id = st.product_id
 WHERE product_class_name = "Class C"
 ORDER BY product_class_name, (retail_price*quantity) DESC, quantity
 LIMIT 2
-# Show the Result
+-- Show the Result
 SELECT *
 FROM answer
